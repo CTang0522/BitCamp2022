c34f16713066fe3a01eb42c29fbc3a34333bbd09
christang522@gmail.com <chris@ChrisLaptop.localdomain>
2022-04-09 13:57:56 -0400

    Differentiate Question Sets and Scores

--- SeeingGreen/src/app/app.component.ts
+++ SeeingGreen/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   score: number = 0;
   show: boolean = true;
   showCards: boolean = false;
+  set: number = 0;
 
   
   constructor() {}
@@ -17,21 +18,31 @@ export class AppComponent {
     this.score += x;
   }
 
+  public async start(ev: any){
+    this.score = 0;
+    this.set = 0;
+    this.show = false;
+    this.showCards = true;
+  }
+
+  public async restart(ev: any){
+    this.score = 0;
+    this.set = 0;
+    this.show = true;
+    this.showCards = false;
+  }
+
   public async buttonPress(ev: any){
     this.show = !this.show;
     this.showCards = !this.showCards;
   }
 
-  public async compost(ev: any){
-    this.counter(5);
+  public async choice(ev: any, value: number, id: number){
+    this.counter(value);
+    this.nextQ(id);
   }
 
-  public async recycle(ev: any){
-    this.counter(0);
+  public async nextQ(id: number){
+    this.set = id;
   }
-
-  public async choice(ev: any, value){
-    this.counter(value)
-  }
-
 }
