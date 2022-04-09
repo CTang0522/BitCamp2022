feb0a6e126b3af8a2063fc14b3c05e0a654109cd
christang522@gmail.com <chris@ChrisLaptop.localdomain>
2022-04-09 13:17:15 -0400

    Explore Differences Between Cards and Buttons

--- SeeingGreen/src/app/app.component.ts
+++ SeeingGreen/src/app/app.component.ts
@@ -6,13 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  score: number = 0;
   show: boolean = true;
+  showCards: boolean = false;
 
+  showRow: boolean = false;
 
   constructor() {}
 
+  public async counter(x: number){
+    this.score += x;
+  }
+
   public async buttonPress(ev: any){
     this.show = !this.show;
+    this.showCards = !this.showCards;
+  }
+
+  public async compost(ev: any){
+    this.counter(5);
+  }
+
+  public async recycle(ev: any){
+    this.counter(0);
   }
 
 }
